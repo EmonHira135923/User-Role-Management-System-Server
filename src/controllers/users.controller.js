441cc67231b2111606ddb03eb1fe32afc8d5c180
EmonHira135923 <emonhossainhira231@gmail.com>
2026-01-05 00:32:05 -0800

    User Creatd Updated and Deletd succesfully and also crud ready

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -4,7 +4,7 @@ import { createUser } from "../models/users.schema.js";
 import cloudinary from "../config/cloudinary.js";
 
 // Create Users
-export const CreateUser = async(req,res) => {
+export const CreateUserController = async(req,res) => {
     try{
         const userCollection = getUsers();
         const {name,email,password,phone,image} = req.body;
@@ -53,7 +53,7 @@ export const CreateUser = async(req,res) => {
 }
 
 // Get Users
-export const GetUsers = async(req,res) => {
+export const GetUsersController = async(req,res) => {
     try{
         const userCollection = getUsers();
         const query =  userCollection.find();
@@ -64,3 +64,114 @@ export const GetUsers = async(req,res) => {
         res.status(400).send(err.message);
     }
 }
+
+// Get User By Id
+export const GetUsersByIdController = async(req,res) => {
+    try{
+        const userCollection = getUsers();
+        const {email} = req.params;
+        const query = {email};
+        const result = await userCollection.findOne(query) ;
+        console.log("result ",result)
+        if(!result){
+            res.status(400).json({
+                message:"Users Not Find",
+                success:false
+            })
+        }
+        res.status(200).send(result);
+    }
+    catch(err){
+        return res.status(404).json({
+            message:"User Not Found Here!!! Try To Registration",
+            success:false
+        })
+    }
+
+
+}
+
+// Update user and also image update also
+export const UpdateUserControllerbyId = async (req, res) => {
+    try {
+        const userCollection = getUsers();
+        const {email} = req.params;
+        const {phone} = req.body;
+        const query = {email};
+
+        const oldUser = await userCollection.findOne(query);
+
+        let image = null;
+
+        if (req.file) {
+
+        if (oldUser?.image?.public_id) {
+        await cloudinary.uploader.destroy(oldUser.image.public_id);
+      }
+
+        const imgResult = await cloudinary.uploader.upload(req.file.path, {
+            folder: "users"
+        });
+
+        image = {
+            url: imgResult.secure_url,
+            public_id: imgResult.public_id
+        };
+        }
+
+        const updatedata = {
+            $set:{
+                phone,
+                image,
+                updatedAt: new Date()
+            }
+        }
+
+        const option = {};
+
+        const result = await userCollection.updateOne(query,updatedata,option);
+
+        res.status(200).json({
+            message: "Updated successfully",
+            success: true,
+            result
+        });
+
+    } catch (err) {
+        res.status(500).json({
+            message: "Update failed",
+            success: false
+        });
+    }
+};
+
+// Delete User and also img in cloudinary
+export const DeleteUserController = async(req,res) => {
+    try{
+    const  userCollection = getUsers();
+    const {email} = req.params;
+    const query = {email};
+
+    // ✅ STEP 1: আগে user খুঁজে বের করি
+    const user = await userCollection.findOne(query);
+
+    // ✅ STEP 2: যদি image থাকে → cloudinary থেকে delete
+    if (user?.image?.public_id) {
+        await cloudinary.uploader.destroy(user.image.public_id);
+    }
+
+    const result = await userCollection.deleteOne(query);
+    res.status(200).json({
+        message:"User Deleted Succesfully",
+        success:true,
+        result});
+    }
+    catch(err){
+        return res.status(400).json({
+            message: "Not deleted data",
+            success:false,
+            err:err.message
+        })
+    }
+
+};
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,13 +1,17 @@
 import express from "express";
-import { CreateUser, GetUsers } from "../controllers/users.controller.js";
+import { CreateUserController, GetUsersController, GetUsersByIdController, UpdateUserControllerbyId, DeleteUserController } from "../controllers/users.controller.js";
 import { CheckUniqueUser, ValidateFields } from "../middlewares/validate.middlewares.js";
 import upload from "../middlewares/multer.config.js";
+import { VerifyToken } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
 
 // Users Route
-router.post("/auth/register",upload.single("image"),ValidateFields,CheckUniqueUser,CreateUser);
-router.get("/auth/register",GetUsers);
+router.post("/auth/register",upload.single("image"),ValidateFields,CheckUniqueUser,CreateUserController);
+router.get("/auth/register",GetUsersController);
+router.get("/auth/users/:email",GetUsersByIdController);
+router.patch("/auth/users/:email",upload.single("image"),VerifyToken,UpdateUserControllerbyId);
+router.delete("/auth/users/:email",VerifyToken,DeleteUserController);
 
 export default router;
