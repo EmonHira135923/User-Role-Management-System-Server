309d70484b59d2f9c0d6298cbb845ec9aa0fd1ef
EmonHira135923 <emonhossainhira231@gmail.com>
2026-01-05 23:28:24 -0800

    Admin Router  added  successfully

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,7 +1,8 @@
 import express from "express";
 import { GetMyProfileController, LoginController, LogoutController } from "../controllers/auth.controller.js";
 import {  ValidateLoginFields } from "../middlewares/validate.middlewares.js";
-import { VerifyToken } from "../middlewares/auth.middleware.js";
+import { verifyAdmin, VerifyToken } from "../middlewares/auth.middleware.js";
+import { DeleteUserByAdmin, GetAllUsers, GetSingleUserbyAdmin, UpdateUserByAdmin } from "../controllers/users.controller.js";
 
 const router = express.Router();
 
@@ -9,5 +10,12 @@ const router = express.Router();
 router.post("/auth/login",ValidateLoginFields,LoginController);
 router.get("/auth/me",VerifyToken,GetMyProfileController);
 router.post("/auth/logout",VerifyToken,LogoutController);
+
+// Auth Routes
+// Admin only
+router.get("/users", VerifyToken, verifyAdmin, GetAllUsers);
+router.get("/users/:email",VerifyToken,verifyAdmin,GetSingleUserbyAdmin);
+router.patch("/users/:email",VerifyToken,verifyAdmin,upload.single("image"),UpdateUserByAdmin);
+router.delete("/users/:email", VerifyToken, verifyAdmin, DeleteUserByAdmin);
 
 export default router
